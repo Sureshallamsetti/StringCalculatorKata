8f84e9387ab475f5d02863a6653445015a9b9bb8
sureshallamsetti <sureshallamsetti9@gmail.com>
2024-11-06 23:51:18 +0530

    Add test for custom delimiter syntax in StringCalculator

--- src/test/java/com/String/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/String/StringCalculator/StringCalculatorTest.java
@@ -32,5 +32,11 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(6, calculator.add("1\n2,3"));  // Supports both commas and newlines
     }
+    @Test
+    public void testAddWithCustomDelimiter() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("//;\n1;2"));  // Custom delimiter ";"
+    }
+
 
 }
