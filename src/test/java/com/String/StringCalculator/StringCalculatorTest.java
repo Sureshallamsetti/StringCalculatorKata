a0bc7ce6d91adc2992f67bab8a837443b7a3ff8f
sureshallamsetti <sureshallamsetti9@gmail.com>
2024-11-06 23:48:54 +0530

    Add test for multiple comma-separated numbers in StringCalculator

--- src/test/java/com/String/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/String/StringCalculator/StringCalculatorTest.java
@@ -21,4 +21,10 @@ public class StringCalculatorTest {
         assertEquals(3, calculator.add("1,2"));  // Sum of two numbers
     }
 
+    @Test
+    public void testAddMultipleNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(10, calculator.add("1,2,3,4"));  // Sum of multiple numbers
+    }
+
 }
